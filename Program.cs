7326fe9968153d85aade5bf4882a5a81e61a4d5a
Leandro Ricardo <leandroricardo@live.com>
2022-01-21 08:27:32 -0300

    Projeto iniciado para teste

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Aula_98__Repositorio_Git_ {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
